d64b3083d08b6fee125b9707d72d437b142d8c56
sk8dcenjoi <trg7527@louisiana.edu>
2019-02-03 20:52:42 -0600

    Enemy addition

---
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public string planktonTag;
+    public Vector3 movementLength;
+
+    private Vector3 previousLocation;
+
+	// Use this for initialization
+	void Start () {
+        previousLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        dropBombs();
+	}
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(planktonTag))
+        {
+            //alter for other scripts
+            /*
+            if (other.gameObject.GetComponent<PlanktonScript>().isMega())
+            {
+                //something happens to enemy
+            }
+            else
+            {
+                //planton lose heath
+            }
+            */
+        }
+    }
+    private void dropBombs()
+    {
+        if ((previousLocation-transform.position).magnitude > movementLength.magnitude)
+        {
+            //Instantiate(Bomb, transform.position, new Quaternion());
+        }
+    }
+}
